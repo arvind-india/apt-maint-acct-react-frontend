683601b27c77d5bfe418b6812f15f9690e283552
Mohan <mohankumar.anna@outlook.com>
2017-12-13 14:48:18 +0530

    impl jwt expiration check

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -17,6 +17,10 @@ class HomePage extends React.Component {
     this.props.dispatch(userActions.logout())
     this.props.dispatch(alertActions.error('You are now Logged out!!'))
   }
+  exitApp() {
+    this.props.dispatch(userActions.logout())
+    this.props.dispatch(alertActions.error('JWT Expired, re-login the Application!'))
+  }
   alertOptions = {
     offset: 14,
     position: 'top right',
@@ -27,8 +31,40 @@ class HomePage extends React.Component {
   show(message) {
     this.msg.show(message)
   }
+  timeNow() {
+    return parseInt(Date.now() / 1000)
+  }
+  isJWTExpired(user) {
+    if(user) {
+      return jwtDecode(user.id_token).exp < this.timeNow()
+    } else {
+      return true
+    }
+  }
+  jwtValidFor(user) {
+    let initData = {
+      differenceInSeconds: 0,
+      remainingHours: 0,
+      remainingMinutes: 0,
+      remainingSeconds: 0
+    }
+    if (!user) return initData;
+    let exp = jwtDecode(user.id_token).exp
+    let now = this.timeNow()
+    if(exp < now) return initData;
+    let diffInSec = exp - now
+    let remainingHours = parseInt(diffInSec / (60*60))
+    let remainingMin = parseInt(diffInSec % (60*60) / 60)
+    let remainingSec = diffInSec - (remainingHours * 60 * 60) - (remainingMin * 60)
+    return {
+      differenceInSeconds: diffInSec,
+      remainingHours: remainingHours,
+      remainingMinutes: remainingMin,
+      remainingSeconds: remainingSec}
+  }
   render() {
     const { user, alert } = this.props
+    const validity = this.jwtValidFor(user)
     return (
       <div>
         <AlertContainer ref={a => this.msg = a} {...this.alertOptions}/>
@@ -38,7 +74,13 @@ class HomePage extends React.Component {
           {user
             ?<Button color="danger" onClick={this.handleLogout}>Logout</Button>
             :<Link to="/login">Login</Link>}
+            <br/>
+          <p>JWT is valid for {validity.remainingHours} hours,
+            {validity.remainingMinutes} minutes,
+            {validity.remainingSeconds} seconds </p>
+          {user && this.isJWTExpired(user) && this.exitApp()}
         </p>
+
       </div>
     )
   }
