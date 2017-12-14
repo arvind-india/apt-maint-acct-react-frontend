3c99b7750c7f2165c3e6e3cf7a4e11ad95b0717e
Mohan <mohankumar.anna@outlook.com>
2017-12-14 10:54:29 +0530

    impl jwt expiration

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -4,10 +4,19 @@ import { connect } from 'react-redux'
 import { Button } from 'reactstrap'
 import AlertContainer from 'react-alert'
 import jwtDecode from 'jwt-decode'
+import moment from 'moment'
 
 import { userActions, alertActions } from '../_actions'
 
 class HomePage extends React.Component {
+
+  alertOptions = {
+    offset: 14,
+    position: 'top right',
+    theme: 'dark',
+    time: 3000,
+    transition: 'scale'
+  }
 
   constructor(props) {
     super(props)
@@ -21,50 +30,16 @@ class HomePage extends React.Component {
     this.props.dispatch(userActions.logout())
     this.props.dispatch(alertActions.error('JWT Expired, re-login the Application!'))
   }
-  alertOptions = {
-    offset: 14,
-    position: 'top right',
-    theme: 'dark',
-    time: 3000,
-    transition: 'scale'
-  }
   show(message) {
     this.msg.show(message)
   }
-  timeNow() {
-    return parseInt(Date.now() / 1000)
-  }
-  isJWTExpired(user) {
-    if(user) {
-      return jwtDecode(user.id_token).exp < this.timeNow()
-    } else {
-      return true
-    }
-  }
-  jwtValidFor(user) {
-    let initData = {
-      differenceInSeconds: 0,
-      remainingHours: 0,
-      remainingMinutes: 0,
-      remainingSeconds: 0
-    }
-    if (!user) return initData;
-    let exp = jwtDecode(user.id_token).exp
-    let now = this.timeNow()
-    if(exp < now) return initData;
-    let diffInSec = exp - now
-    let remainingHours = parseInt(diffInSec / (60*60))
-    let remainingMin = parseInt(diffInSec % (60*60) / 60)
-    let remainingSec = diffInSec - (remainingHours * 60 * 60) - (remainingMin * 60)
-    return {
-      differenceInSeconds: diffInSec,
-      remainingHours: remainingHours,
-      remainingMinutes: remainingMin,
-      remainingSeconds: remainingSec}
+  isJWTExpired(token) {
+      let tokenExpiration = jwtDecode(token).exp
+      // if token is about to expire in the next 30 seconds...
+      return moment.unix(tokenExpiration) - moment(Date.now()) < 30
   }
   render() {
     const { user, alert } = this.props
-    const validity = this.jwtValidFor(user)
     return (
       <div>
         <AlertContainer ref={a => this.msg = a} {...this.alertOptions}/>
@@ -75,11 +50,7 @@ class HomePage extends React.Component {
             ?<Button color="danger" onClick={this.handleLogout}>Logout</Button>
             :<Link to="/login">Login</Link>}
             <br/>
-          <p>JWT is valid for {validity.remainingHours} hours,
-            {validity.remainingMinutes} minutes,
-            {validity.remainingSeconds} seconds </p>
-          {user && this.isJWTExpired(user) && this.exitApp()}
-          JWT Token: {user && user.id_token}
+          {user && this.isJWTExpired(user.id_token) && this.exitApp()}
         </p>
 
       </div>
