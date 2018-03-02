3b31d7761600d212af3a79e007fac43a324938ff
Mohan <mohankumar.anna@outlook.com>
2018-03-02 10:48:15 +0530

    modified timeout parameter in axios

--- src/_services/axios.instance.js
+++ src/_services/axios.instance.js
@@ -9,7 +9,7 @@ function instance() {
   let user = JSON.parse(sessionStorage.getItem('user'))
   return axios.create({
     baseURL: process.env.REACT_APP_API_URL+'/api',
-    timeout: 1000,
+    timeout: 10000,
     headers: {
       'Content-Type': 'application/json',
       'x-access-token': user?user.id_token:null
