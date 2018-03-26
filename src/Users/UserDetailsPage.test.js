c9bf39b4d2231016908c1ccfff5c5dca2244f0d2
Mohan <mohankumar.anna@outlook.com>
2018-03-26 22:00:30 +0530

    added second test in userpage

--- src/Users/UserDetailsPage.test.js
+++ src/Users/UserDetailsPage.test.js
@@ -4,52 +4,101 @@ import React from 'react'
 import { shallow } from 'enzyme'
 
 import { UserDetails } from './UserDetailsPage'
-const mockUser = {
-  id: 1,
-  name: 'user1',
-  first_name:'user1',
-  last_name: 'test',
-  email: 'user1@test.com'
-}
 
-const mockProps = {
-  alert: {message: "testingAlert"},
-  authzn: {
-    allowsAdd: true,
-    allowsEdit: true,
-    allowsDelete: true,
-    condition: ''
+const mockUsers = [
+  {
+    id: 1,
+    name: 'user1',
+    first_name:'user1',
+    last_name: 'test',
+    email: 'user1@test.com'
   },
-  clearAlert: jest.fn(),
-  getById: jest.fn(),
-  saveChanges: jest.fn(),
-  error: jest.fn()
-}
-const mockLocation = {
-  state: {
-    model: mockUser,
-    title: 'User Details',
-    module: '',
-    infos: [],
-    password: 'secret',
-    confirmPassword: 'secret',
-    passwordChanged: false,
-    passwordMatches: false,
-    submitted: false,
-    touched: false,
-    adding: false,
-    title: 'Mock Test: User Details'
+  {
+    id: 2,
+    name: 'user2',
+    first_name:'user2',
+    last_name: 'test',
+    email: 'user2@test.com'
   }
-}
 
-const mock = {
-  userName: {name: 'name', value: mockUser.name},
-  firstName: {name: 'first_name', value: mockUser.first_name},
-  lastName: {name: 'last_name', value: mockUser.last_name},
-  email: {name: 'email', value: mockUser.email }
-}
+]
 
-const component = shallow(<UserDetails {...mockProps} location={mockLocation}/>)
+const mockProps = [
+  {
+    alert: {message: "testingAlert"},
+    authzn: {
+      allowsAdd: true,
+      allowsEdit: true,
+      allowsDelete: true,
+      condition: ''
+    },
+    clearAlert: jest.fn(),
+    getById: jest.fn(),
+    saveChanges: jest.fn(),
+    error: jest.fn()
+  },
+  {
+    alert: {message: "testingAlert"},
+    authzn: {
+      allowsAdd: true,
+      allowsEdit: true,
+      allowsDelete: true,
+      condition: ''
+    },
+    clearAlert: jest.fn(),
+    getById: jest.fn(),
+    saveChanges: jest.fn(),
+    error: jest.fn()
+  }
+]
+const mockLocations = [
+  {
+    state: {
+      model: mockUsers[0],
+      title: 'Mock Test: User1 Details',
+      module: '',
+      infos: [],
+      password: 'secret',
+      confirmPassword: 'secret',
+      passwordChanged: false,
+      passwordMatches: false,
+      submitted: false,
+      touched: false,
+      adding: false
+    }
+  },
+  {
+    state: {
+      model: mockUsers[1],
+      title: 'Mock Test: User2 Details',
+      module: '',
+      infos: [],
+      password: 'secret',
+      confirmPassword: 'secret',
+      passwordChanged: false,
+      passwordMatches: false,
+      submitted: false,
+      touched: false,
+      adding: false
+    }
+  }
+]
+
+const mocks = [
+  {
+    userName: {name: 'name', value: mockUsers[0].name},
+    firstName: {name: 'first_name', value: mockUsers[0].first_name},
+    lastName: {name: 'last_name', value: mockUsers[0].last_name},
+    email: {name: 'email', value: mockUsers[0].email }
+  },
+  {
+    userName: {name: 'name', value: mockUsers[1].name},
+    firstName: {name: 'first_name', value: mockUsers[1].first_name},
+    lastName: {name: 'last_name', value: mockUsers[1].last_name},
+    email: {name: 'email', value: mockUsers[1].email }
+  }
+]
+const component = shallow(<UserDetails {...mockProps[0]} location={mockLocations[0]}/>)
 
 describe('UsersPage params test', () => {
   it('should render UsersPage without crashing', () => {
@@ -92,17 +141,29 @@ describe('UsersPage params test', () => {
 })
 
 
-describe('UserDetailsPage events test', () => {
+describe('UserDetailsPage with NO INFOS events test', () => {
   it('should call getById on submit and no crash', () => {
     component.find('#userDetailsForm').simulate('submit', {preventDefault() {}})
-    expect(mockProps.getById.mock.calls.length).toEqual(1)
+    expect(mockProps[0].getById.mock.calls.length).toEqual(1)
   })
   it('should be called with required states as arguments', () => {
-    component.find('#userName').simulate('change', {target: mock.userName})
-    component.find('#firstName').simulate('change', {target: mock.firstName})
-    component.find('#lastName').simulate('change', {target: mock.lastName})
-    component.find('#email').simulate('change', {target: mock.email})
+    component.find('#userName').simulate('change', {target: mocks[0].userName})
+    component.find('#firstName').simulate('change', {target: mocks[0].firstName})
+    component.find('#lastName').simulate('change', {target: mocks[0].lastName})
+    component.find('#email').simulate('change', {target: mocks[0].email})
     component.find('#userDetailsForm').simulate('submit', {preventDefault() {}})
-    expect(mockProps.saveChanges.mock.calls[1][0]).toEqual(mockUser)
+    expect(mockProps[0].saveChanges.mock.calls[1][0]).toEqual(mockUsers[0])
+  })
+})
+
+const component2 = shallow(<UserDetails {...mockProps[1]} location={mockLocations[1]}/>)
+describe('UserDetailsPage with WITH INFOS events test', () => {
+  it('should be called with required states as arguments', () => {
+    component2.find('#userName').simulate('change', {target: mocks[1].userName})
+    component2.find('#firstName').simulate('change', {target: mocks[1].firstName})
+    component2.find('#lastName').simulate('change', {target: mocks[1].lastName})
+    component2.find('#email').simulate('change', {target: mocks[1].email})
+    component2.find('#userDetailsForm').simulate('submit', {preventDefault() {}})
+    expect(mockProps[1].saveChanges.mock.calls[0][0]).toEqual(mockUsers[1])
   })
 })
