a06d33c9db326062d438b1f49bb5a90f6dd1486b
Mohan <mohankumar.anna@outlook.com>
2018-08-15 13:34:23 +0530

    restored to old content

--- src/_services/axios.instance.js
+++ src/_services/axios.instance.js
@@ -7,12 +7,12 @@ export const axiosClient={
 
 function instance() {
   let user = JSON.parse(sessionStorage.getItem('user'))
-/*
+
   let host = process.env.NODE_ENV === 'production' ?
               window.location.origin :
               process.env.REACT_APP_API_URL
-*/
-  let host = process.env.REACT_APP_API_URL
+
+  //let host = process.env.REACT_APP_API_URL
   return axios.create({
     baseURL: host+'/api',
     timeout: 10000,
